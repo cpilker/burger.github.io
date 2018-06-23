97e200593ab7e348133b4a42caa3710f2bd8964f
Chad Pilker <chad.pilker@gmail.com>
2018-06-23 11:41:10 -0400

    Add files via upload

--- config/connection.js
+++ config/connection.js
@@ -12,7 +12,7 @@ else {
   connection = mysql.createConnection({
     connection = mysql.createConnection({
     host: "localhost",
-    //port: 3306,
+    port: 3306,
     user: "root",
     password: process.env.password,
     database: "burgers_db"
